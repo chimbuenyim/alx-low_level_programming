0b07b4c1276043b9b82800d9c42a89d7ff0fc23e
Emeka Duruji <obioha.duruji@gmail.com>
2022-09-14 00:01:31 +0100

    PutChar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - Prints a message.
+ * main - Prints _putchar as a message.
  *
  * Return: Always 0 (Success)
  */
@@ -18,4 +18,3 @@ int main(void)
 	_putchar('\n');
 	return (0);
 }
-
